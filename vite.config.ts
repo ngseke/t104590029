010d73c01879c58d75bd3570d33f6a358cff0dee
Huang Xingqiao <a92304a92304@gmail.com>
2021-06-13 14:04:10 +0800

    chore: Minify Html

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import WindiCSS from 'vite-plugin-windicss'
+import { minifyHtml } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     reactRefresh(),
     WindiCSS(),
+    minifyHtml(),
   ],
   base: './',
 })
